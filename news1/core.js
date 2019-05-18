46c7a9b7ca8722d8472dbbc6bbf2768427f82188
Blake Arnold <bablake@gmail.com>
2019-05-17 22:12:11 -0400

    updated

---
+++ news1/core.js
@@ -0,0 +1,4 @@
+/*! updated; 05-09-2019 11:27 AM **/
+
+
+!function(t){if(!(window.FNC&&window.FNC.GEO?true:false)){var c=(new Date).getTime();document.write('<script src="https://static.foxnews.com/static/orion/scripts/core/utils/geo.js?cb='+c+'"><\/script>')}var e,s="https://global.fncstatic.com/static/isa/core-app.js?v=v19";try{"fts"!==((e=document.querySelector('meta[name="prism.channel"]'))&&e.content?e.content:null)||/\/core\-app\.?(fts)/.test(s)||(s=s.replace(/\/core\-app\./,"/core-app.fts."))}catch(t){}document.write('<script src="'+s+'"><\/script>')}();
---
+++ news1/loader.js
@@ -0,0 +1,4 @@
+/*! updated; 05-08-2019 05:10 PM **/
+
+
+!function(FNC,$){$(document).ready(function(){$(window).scrollTop(0)}),$(window).scrollTop(0),FNC.Loader&&FNC.Loader.load(["/static/orion/scripts/core/pages/articles.new/ag.app.js"])}(window.FNC||{},window.jQuery);
